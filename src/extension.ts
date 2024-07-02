f17ed85e07e02fbb5c04145dd016dd4ac7cc48b1
Stacksmasher <nicolai.sandmann@gmx.net>
2024-07-02 23:54:33 +0200

    Added hotkey bar

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
 
 const hotkeyMap: Map<number, string> = new Map();
+var hotkeyBarMap: Map<number, vscode.StatusBarItem> = new Map();
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "hotkey" is now active!');
@@ -9,23 +10,37 @@ export function activate(context: vscode.ExtensionContext) {
 		const assignCommand = `hotkey.assign${i}`;
 		const jumpCommand = `hotkey.jump${i}`;
 
+		// Register command specified in package.json (Standard: <cntrl>+<number>) to assign a hotkey to the currently opened file.
 		context.subscriptions.push(vscode.commands.registerCommand(assignCommand, () => {
 			assignHotkey(i);
 		}));
 
+		// Register command specified in package.json (Standard: <shift>+<number>) to jump to the file assigned to the hotkey previously.
 		context.subscriptions.push(vscode.commands.registerCommand(jumpCommand, () => {
 			jumpToHotkey(i);
 		}));
 	}
 }
 
+function getFileNameFromUri(uri: string) {
+	return vscode.Uri.parse(uri, true).path.split('/').pop() || '';
+}
+
 function assignHotkey(number: number) {
 	const editor = vscode.window.activeTextEditor;
 	if (editor) {
 		const filePath = editor.document.uri.toString();
+		const fileName = getFileNameFromUri(filePath);
+		if (hotkeyMap.has(number)) {
+			const previousFileName = getFileNameFromUri(hotkeyMap.get(number) || '');
+			vscode.window.showInformationMessage(`Reassigned hotkey ${number} from ${previousFileName} to ${fileName}`);
+		} else {
+			vscode.window.showInformationMessage(`Assigned hotkey ${number} to ${fileName}`);
+		}
 		hotkeyMap.set(number, filePath);
-		vscode.window.showInformationMessage(`Assigned hotkey ${number} to ${filePath}`);
+		updateHotkeyBarItem(number, fileName);
 	} else {
+		// This code should be unreachable because the command is disabled when there is no active editor.
 		vscode.window.showErrorMessage('No active editor to assign a hotkey.');
 	}
 }
@@ -40,6 +55,16 @@ function jumpToHotkey(number: number) {
 	} else {
 		vscode.window.showErrorMessage(`No file assigned to hotkey ${number}`);
 	}
+}
+
+function updateHotkeyBarItem(number: number, fileName: string) {
+	if (hotkeyBarMap.has(number)) {
+		hotkeyBarMap.get(number)?.dispose();
+	}
+	let hotkeyBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 10 - number)
+	hotkeyBarMap.set(number, hotkeyBar);
+	hotkeyBar.text = `${number}: ${fileName}`;
+	hotkeyBar.show();
 }
 
 export function deactivate() { }
